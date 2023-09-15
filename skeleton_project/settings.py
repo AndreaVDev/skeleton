21435db208a9fa5f491c59806d7127268fc84419
AndreaVDev <andrea.verdura3@gmail.com>
2023-09-15 17:49:29 +0200

    Added django boostrap and crispy forms

--- skeleton_project/settings.py
+++ skeleton_project/settings.py
@@ -37,11 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "accounts.apps.AccountsConfig",
     "sktmp.apps.SktmpConfig",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",

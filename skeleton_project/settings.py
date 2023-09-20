5f7ba3d5daa9b1c6dab01a72339d6d2db33eb86b
AndreaVDev <andrea.verdura3@gmail.com>
2023-09-20 17:46:16 +0200

    Added environment variable for configurations in settings.py

--- skeleton_project/settings.py
+++ skeleton_project/settings.py
@@ -11,23 +11,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4)zebb0133@--+jig83l4#(d_u1!c3_cj=&mv4xq8!%)1iaqvd"
+# SECRET_KEY = "django-insecure-4)zebb0133@--+jig83l4#(d_u1!c3_cj=&mv4xq8!%)1iaqvd"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+DEBUG = env.bool("DJANGO_DEBUG")
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,7 +46,6 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "sktmp.apps.SktmpConfig",
 ]
-
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -81,16 +81,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "skeleton_project.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    "default": {
 #        "ENGINE": "django.db.backends.sqlite3",
 #        "NAME": BASE_DIR / "db.sqlite3",
 #    }
-#}
+# }
 
 DATABASES = {
     "default": {
@@ -102,7 +101,6 @@ DATABASES = {
         "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -122,7 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -133,7 +130,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -147,12 +143,10 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 ACCOUNT_LOGOUT_REDIRECT = "home"
-
 
 SITE_ID = 1
 AUTHENTICATION_BACKENDS = (
